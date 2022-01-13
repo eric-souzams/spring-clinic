334b15c4935185796d49b983a2789e93db779ded
Eric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2022-01-13 18:22:47 -0300

    wip: change spring version and add some base files

---
+++ src/main/java/com/security/springclinic/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.security.springclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}	
+}
