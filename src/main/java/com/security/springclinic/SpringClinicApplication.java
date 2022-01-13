931926e62441ce114a16d077fca918bb5faff21d
Eric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2022-01-13 16:43:26 -0300

    Init clinic project

---
+++ src/main/java/com/security/springclinic/SpringClinicApplication.java
@@ -0,0 +1,13 @@
+package com.security.springclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/springclinic/SpringClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.springclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
